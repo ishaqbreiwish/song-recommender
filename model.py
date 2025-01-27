6b8a8c7d3f69daa2c9f54b294eb7603b23cd1438
Ishaq Breiwish <ishaq.breiwish@gmail.com>
2025-01-27 15:17:02 -0500

    Udpated user data

--- model.py
+++ model.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     triplets_file = 'https://static.turi.com/datasets/millionsong/10000.txt'
     songs_metadata_file = 'https://static.turi.com/datasets/millionsong/song_data.csv'
 
-    song_df_1 = pd.read_table(triplets_file, header=None)
+
     song_df_1.columns = ['user_id', 'song_id', 'listen_count']
 
     song_df_2 = pd.read_csv(songs_metadata_file)
